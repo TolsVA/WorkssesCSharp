8a563aceb66eb3e10050bd120f0c0c3b3c67fa4d
Виталий Толстихин <himtol@yandex.ru>
2023-10-08 16:57:07 +0700

    add task 1 days of the week

---
+++ Seminars/Sem_1/task_1_days_of_the_week/Program.cs
@@ -0,0 +1,4 @@
+﻿using System.ComponentModel.DataAnnotations;
+string[] den = {"Понедельник", "Вторник", "Среда", "Четверг", 
+"Пятница", "Субота", "Воскресенье"};
+System.Console.WriteLine(den[Convert.ToInt32(Console.ReadLine()) -1]);
