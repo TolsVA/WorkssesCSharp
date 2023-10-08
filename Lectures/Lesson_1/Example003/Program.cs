2d05ea38f9d3ead497c6a38ce1dc3fe8347d2699
Виталий Толстихин <himtol@yandex.ru>
2023-10-08 16:41:14 +0700

    add Example003 Calculator

---
+++ Lectures/Lesson_1/Example003/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+class Program{
+    static void Main(string[] args) {
+        double a = new Random().Next(1, 10);
+        double b = new Random().Next(1, 10);
+        Calculator calculator = new Calculator();
+        Console.Write(calculator.div(a, b));
+        Console.WriteLine("");
+        Console.Write(calculator.work(a, b));
+        Console.WriteLine("");
+        Console.Write(calculator.sum(a, b));
+    }
+}
