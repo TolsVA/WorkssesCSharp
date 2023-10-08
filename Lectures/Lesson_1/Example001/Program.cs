833b1afa2849c136ac2a33ed8107c688ba8bacef
Виталий Толстихин <himtol@yandex.ru>
2023-10-08 14:44:41 +0700

    add Example001 if()->else

---
+++ Lectures/Lesson_1/Example001/Program.cs
@@ -0,0 +1,10 @@
+﻿using System.Dynamic;
+Console.WriteLine("Введите имя пользователя!");
+string? name = Console.ReadLine();
+
+if(name?.ToLower() == "masha") { // name.ToLower() Переводит све символы в нижний регистр
+    System.Console.WriteLine("Ура это же МАША"); 
+} else {
+    System.Console.WriteLine($"Привет {name}");
+}
+
