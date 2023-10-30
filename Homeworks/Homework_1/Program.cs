d05331f5c65ba3f782e350b04c71eaaa45c5b776
Виталий Толстихин <himtol@yandex.ru>
2023-10-31 00:40:31 +0700

    add Homework_1 (factorial)

--- Homeworks/Homework_1/Program.cs
+++ Homeworks/Homework_1/Program.cs
@@ -1,1 +1,3 @@
 ﻿Console.WriteLine("Для решения данного домашнего задания вам необходимо воспользоваться сервисом автоматической проверки написанного кода на сайте gb в личном кабинете на странице домашнего задания к уроку");
+System.Console.WriteLine(factorial(4));
+int factorial(int n) { if (n == 1) return 1; else return n * factorial(n - 1); }
